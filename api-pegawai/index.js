036a09823d6b47d13c112f6fbc6eb71235dad5c7
Pudintea <najzmitea@gmail.com>
2024-04-25 08:56:14 +0700

    Update index.js

--- api-pegawai/index.js
+++ api-pegawai/index.js
@@ -10,27 +10,32 @@ const response = require('./response')
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Selamat Datang !')
 })
 
 app.get('/find', (req, res) => {
     const nip = req.query.nip;
-    const sql = `SELECT nip FROM t_pegawai WHERE nip = ${nip}`;
-    db.query(sql, [nip], (error, result) => {
-        if (error) {
-            throw error;
-        }
-        if (result.length === 1) {
-            // console.log(result[0].nip);
-            // res.send(result[0]);
-            response(200, 'success', res)
-        }else if(result.length > 1){
-            response(204, 'notcontent', res)
-        } else {
-            //res.status(404).send("Data not found");
-            response(404, 'notfound', res)
-        }
-    });
+    if (!nip) {
+        res.send('Tidak ada data yang dikirim !');
+    } else {
+        const sql = `SELECT nip FROM t_pegawai WHERE nip = ${nip}`;
+        db.query(sql, [nip], (error, result) => {
+            if (error) {
+                throw error;
+            }
+
+            if (result.length === 1) {
+                //res.status(200).send('success');
+                response(200, 'success', res)
+            } else if (result.length > 1) {
+                //res.status(204).send('notcontent');
+                response(204, 'notcontent', res)
+            } else {
+                //res.status(404).send("Data not found");
+                response(404, 'Data not found', res)
+            }
+        });
+    }
 });
 
 app.listen(port, () => {
