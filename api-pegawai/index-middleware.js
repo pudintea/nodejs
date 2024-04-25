ff5e0711fa7f8197aa763c06c24c7240ca3df9b8
Pudintea <najzmitea@gmail.com>
2024-04-25 09:24:29 +0700

    Create index-middleware.js

---
+++ api-pegawai/index-middleware.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+const db = require('./connection')
+const response = require('./response')
+
+// ROUTE UTAMA
+app.get('/', (req, res) => {
+    res.send('Selamat Datang !')
+})
+
+//Middleware
+const satpam = (req, res, yunext) => {
+    const key = "12110"
+    if (req.query.kkk == key) {
+        yunext();
+    }else{
+        res.status(404).send("Lapor dulu 1x24 Jam !");
+    }
+}
+
+// CARI NIPNYA
+app.get('/find', satpam, (req, res) => {
+    const nip = req.query.nip;
+    if (!nip) {
+        res.send('Tidak ada data yang dikirim !');
+    } else {
+        const sql = `SELECT nip FROM t_pegawai WHERE nip = ${nip}`;
+        db.query(sql, [nip], (error, result) => {
+            if (error) {
+                throw error;
+            }
+
+            if (result.length === 1) {
+                //res.status(200).send('success');
+                response(200, 'success', res)
+            } else if (result.length > 1) {
+                //res.status(204).send('notcontent');
+                response(204, 'notcontent', res)
+            } else {
+                //res.status(404).send("Data not found");
+                response(404, 'Data not found', res)
+            }
+        });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
