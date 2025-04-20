76a003550c85c7b880d3ced9d6b47c019cd449ac
Pudintea <najzmitea@gmail.com>
2025-04-20 08:46:33 +0700

    Create autoload.php

---
+++ codeigniter-frontend/config/autoload.php
@@ -0,0 +1,48 @@
+<?php
+defined('__NAJZMI_PUDINTEA__') OR exit('No direct script access allowed');
+
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Libraries
+| -------------------------------------------------------------------
+| These are the classes located in system/libraries/ or your
+| application/libraries/ directory, with the addition of the
+| 'database' library, which is somewhat of a special case.
+|
+| Prototype:
+|
+|	$autoload['libraries'] = array('database', 'email', 'session');
+|
+| You can also supply an alternative library name to be assigned
+| in the controller:
+|
+|	$autoload['libraries'] = array('user_agent' => 'ua');
+*/
+$autoload['libraries'] = array('Template','form_validation','session','pudin_dev');
+
+/*
+| -------------------------------------------------------------------
+|  Auto-load Libraries
+| -------------------------------------------------------------------
+| These are the classes located in system/libraries/ or your
+| application/libraries/ directory, with the addition of the
+| 'database' library, which is somewhat of a special case.
+|
+| Prototype:
+|
+|	$autoload['libraries'] = array('database', 'email', 'session');
+|
+| You can also supply an alternative library name to be assigned
+| in the controller:
+|
+|	$autoload['libraries'] = array('user_agent' => 'ua');
+*/
+$autoload['libraries'] = array('Template','form_validation','session','pudin_dev');
+
+
+/*
+| -------------------------------------------------------------------
+|  END PUDIN.MY.ID
+| -------------------------------------------------------------------
+*/
